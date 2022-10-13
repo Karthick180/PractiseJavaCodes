beeeda37b33d4bb36c933ddaa6b3657acbab1f32
Karthick180 <karthickthalamahendran@gmail.com>
2022-10-13 10:32:57 +0530

    Done with repetition..

--- Practise/src/main/java/SimpleCalc/SimpleCalculator.java
+++ Practise/src/main/java/SimpleCalc/SimpleCalculator.java
@@ -3,67 +3,81 @@ package SimpleCalc;
 import java.util.Scanner;
 
 class CalcOperations{
-    public static int addition(int a, int b){
+//    public static int addition(int a, int b){return a+b;}
+    protected final float addition(float a, float b){
         return a+b;
     }
-    public static int subtraction(int a, int b){
+//    public static int subtraction(int a, int b){return a-b;}
+    protected final float subtraction(float a, float b){
         return a-b;
     }
-    public static int multiplication(int a, int b){
+//    public static int multiplication(int a, int b){
+//        return a*b;
+//    }
+    protected final float multiplication(float a, float b){
         return a*b;
     }
-    public static int division(int a, int b){
+//    public static int division(int a, int b){
+//        return a/b;
+//    }
+    protected final float division(float a, float b){
         return a/b;
     }
 }
 
 public class SimpleCalculator {
     public static void main(String[] args) {
+        CalcOperations operations = new CalcOperations();
 
-
+        float firstNumber;
+        float secondNumber;
+        String repeatOperation = null;
         Scanner scanner = new Scanner(System.in);
-        // Selecting operations
-        System.out.println("Choose your operations\n 1.add\n 2.sub\n 3.mul\n 4.div");
 
-        System.out.print("Enter your choice: ");
-        String choiceOfOperation = scanner.nextLine();
 
-        // Giving input values for operations
-        System.out.print("Enter First Number: ");
-        int firstNumber = scanner.nextInt();
+            // Selecting operations
+            System.out.println("Choose your operations\n 1.add\n 2.sub\n 3.mul\n 4.div");
 
-        System.out.print("Enter Second Number: ");
-        int secondNumber = scanner.nextInt();
+            System.out.print("Enter your choice: ");
+            String choiceOfOperation = scanner.nextLine();
 
-        switch (choiceOfOperation.toLowerCase()){
-            default:
-                System.out.println("Enter valid response from the operations correctly");
-            case "add":
-                System.out.println(CalcOperations.addition(firstNumber,secondNumber));
-                break;
-            case "sub":
-                System.out.println(CalcOperations.subtraction(firstNumber,secondNumber));
-                break;
-            case "mul":
-                System.out.println(CalcOperations.multiplication(firstNumber,secondNumber));
-                break;
-            case "div": // figured out new problem in catch printing
-                try{
-                if (secondNumber!=0) {
-                    System.out.println(CalcOperations.division(firstNumber, secondNumber));
+            do {
+                // Giving input values for operations
+                System.out.print("Enter First Number: ");
+                firstNumber = scanner.nextFloat();
+
+                System.out.print("Enter Second Number: ");
+                secondNumber = scanner.nextFloat();
+
+                switch (choiceOfOperation.toLowerCase()) {
+                    default:
+                        System.out.println("Enter valid response from the operations correctly");
+                    case "add":
+                        System.out.println(operations.addition(firstNumber, secondNumber));
+                        break;
+                    case "sub":
+                        System.out.println(operations.subtraction(firstNumber, secondNumber));
+                        break;
+                    case "mul":
+                        System.out.println(operations.multiplication(firstNumber, secondNumber));
+                        break;
+                    case "div": // figured out new problem in catch printing
+                        try {
+                            if(secondNumber!=0)
+                                System.out.println(operations.division(firstNumber, secondNumber));
+                            else
+                                System.out.println("The dividing number must be greater than \"zero\"");
+                        } catch (ArithmeticException e) {
+                            System.err.println(" ");
+                        }
+                        break;
+
                 }
-                else
-                    System.err.print("Numbers can't be divisible by zero");
-                }catch(ArithmeticException e){
-                    e.printStackTrace();
-                }
+                System.out.print("Do you want to repeat again [y/n]");
+                repeatOperation = scanner.next();
+            }while(repeatOperation.equalsIgnoreCase("y"));
 
-
-                break;
-
-        }
-
-
+        System.err.println("\"Thank you for using me sir\", Have a great day :). \nbye");
 
     }
 }
